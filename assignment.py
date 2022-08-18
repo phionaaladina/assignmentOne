80e44617a9ddd9d3b1a90456a9a0cfe631d1f497
Phiona Aladina <phionaaladina@gmail.com>
2022-08-18 16:03:38 +0300

    first commit

---
+++ assignment.py
@@ -0,0 +1,10 @@
+#creating variables and assigning values
+my_name = "Aladina"
+
+my_age = 26
+
+my_weight = 50.0
+
+
+#print statement
+print("My favorite name is " + my_name + ", I am " + str(my_age) + " years, " + "I weigh " + str(my_weight) + " Kgs")
